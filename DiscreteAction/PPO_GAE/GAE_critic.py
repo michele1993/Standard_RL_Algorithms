b88614f62a7d4c620b7a9cf8fd1a64a4868c9e33
michele1993 <michele.garibbo@bristol.ac.uk>
2022-01-03 18:18:00 +0000

    Update GAE_critic.py

--- DiscreteAction/PPO_GAE/GAE_critic.py
+++ DiscreteAction/PPO_GAE/GAE_critic.py
@@ -25,12 +25,10 @@ class Critic_NN(nn.Module):
 
     def GAE(self, values, last_v_val,rewards, masks):
 
-        #returns = torch.empty(0)
         returns = []
 
         values = values + [last_v_val]
 
-        #advantage = torch.empty(0)
         gae = torch.tensor([0])
 
         # start from last t step and go backward, multiplying by lambda and gamma at each step the GAE, to eansure accurate weighting for each TD_t error
@@ -39,15 +37,6 @@ class Critic_NN(nn.Module):
             delta = rewards[i] + self.gamma * values[i+1] * masks[i] - values[i] # values has one extra entry that rewards due to final state, need to values[i+1].detach() ?
             gae = delta + self.lambda_p * self.gamma * masks[i] * gae
 
-            #advantage = torch.cat([advantage, gae]) # return = gae_t + v(s_t), not sure why, I think because that your discounted/weighted return target, while in gae it's the TD error, so need values[:-1].detach()
-
-            #returns = torch.cat([returns, gae + values[i].detach()])  # return = gae_t + v(s_t), not sure why, I think because that your discounted/weighted return target, while in gae it's the TD error, so need values[:-1].detach()
             returns.insert(0,gae + values[i])
 
-        #returns = torch.flip(advantage, (0,)) + values[:-1] # values has one extra element for final state, because to compute return you added V(s_t)
-        #returns = torch.flip(torch.stack(returns, dim=0), (0,))
-
-        #advantage = torch.flip(returns, (0,)) - values[:-1]
-        #advantage = returns - values[:-1]  # values has one extra element for final state, because to compute return you added V(s_t)
-
-        return returns #, (advantage - torch.mean(advantage))/(torch.std(advantage) + 1e-10).detach() # add residual not to divide by 0 and normalise advantage to smooth learning
+        return returns
